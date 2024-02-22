43342f8999d6e48a0c0919ca90ee98511d4f0c75
wizzthe3rd <wesleyosayande@gmail.com>
2024-02-22 14:32:35 +0000

    clock logic

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let startTime = 0;
+let time = startTime * 60;
+const countdownEL = document.getElementById('countdown');
+
+
+function startTimer(){
+    setInterval(updateTime, 1000);  //calls function every second
+}
+
+function updateTime(){
+    const minutes = Math.floor(time / 60); //rounds down
+    let seconds = time % 60;
+
+    countdownEL.innerHTML = `${minutes}: ${seconds}`
+    time--;
+}
 
 
 function changeMenuIcon(x){
