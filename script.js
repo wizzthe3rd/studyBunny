780e3b58e6a11d4ccaa90e86aed29689308615ef
wizzthe3rd <wesleyosayande@gmail.com>
2024-02-26 00:36:40 +0000

    after completion of timer, user is rewarded

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-let currency = 0;
-
+let currency = 100;
+document.getElementById('currency').innerHTML = currency;
 
 document.getElementById('play-button').addEventListener('click', showTimeModal);
 document.querySelector('.hamburger-menu').addEventListener('click', toggleMenu);
 document.querySelector('.shop-icon').addEventListener('click', toggleShop);
 document.querySelector('.shop-modal-close').addEventListener('click', toggleShop);
 
+document.querySelector('.currency-modal-close').addEventListener('click', function(){
+    toggleCurrencyMessage();
+    document.querySelector('.currency-modal').removeChild('currencyMessage');
+})
+
 document.querySelector('.time-modal-submit').addEventListener('click', function(){
     startTimer();
     showTimeModal();
 });
-
-
 
 
 function startTimer(){
@@ -41,8 +44,17 @@ function startTimer(){
 
         document.getElementById('countdown').innerHTML = displayMin + ':' + displaySec;
         if (totalTime <= 0){
-            currency+= Math.floor(currencyTime / 2);
+            let amountEarned = Math.floor(currencyTime / 2)
+            currency+= amountEarned;
             document.getElementById('currency').innerHTML = currency;
+            let modalContainer = document.querySelector('.currency-modal');
+            let modalElement = document.createElement("currencyMessage");
+            if (amountEarned > 0){
+                toggleCurrencyMessage();
+                modalElement.innerHTML += `<h3>Well done! You earned <span>${amountEarned}</span> carrots!</h3>`;
+                modalContainer.appendChild(modalElement);
+            }
+
             clearInterval(timer); //stops timer setInterval
             document.getElementById('countdown').innerHTML = "00:00"
             document.querySelector('.countdown-background').classList.toggle('active')
@@ -59,19 +71,24 @@ function startTimer(){
 
 function showTimeModal(){
     document.getElementById('time-modal').classList.toggle('active');
-    document.getElementById('overlay').classList.toggle('active')
+    document.getElementById('overlay').classList.toggle('active');
 }
 
 function toggleShop(){
-    document.querySelector('.shop-modal').classList.toggle('active')
-    document.getElementById('overlay').classList.toggle('active')
+    document.querySelector('.shop-modal').classList.toggle('active');
+    document.getElementById('overlay').classList.toggle('active');
 }
 
 function toggleMenu(){
-    document.querySelector('.menu').classList.toggle('active')
-    document.getElementById('overlay').classList.toggle('active')
+    document.querySelector('.menu').classList.toggle('active');
+    document.getElementById('overlay').classList.toggle('active');
 }
 function toggleModal(){
-    document.getElementById('time-modal').classList.toggle('active')
-    document.getElementById('overlay').classList.toggle('active')
+    document.getElementById('time-modal').classList.toggle('active');
+    document.getElementById('overlay').classList.toggle('active');
+}
+
+function toggleCurrencyMessage(){
+    document.querySelector('.currency-modal').classList.toggle('active');
+    document.getElementById('overlay').classList.toggle('active');
 }
