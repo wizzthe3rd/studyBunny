5b6d9aab3f173528028cce2c9e92a0bd779e9315
wizzthe3rd <wesleyosayande@gmail.com>
2024-03-06 11:26:34 +0000

    green changes

--- script.js
+++ script.js
@@ -1,59 +1,118 @@
-let currency = 100;
+
+let initialStudyInfo = {
+}
+
+const quoteDisplay = document.querySelector('#quote');
+const timePopUp = document.querySelector('#time-modal');
+const overlay = document.querySelector('#overlay');
+const currencyMessage = document.querySelector('.currency-modal');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const shopIcon = document.querySelector('.shop-icon');
+const currencyEl = document.querySelector('#currency'); //element to display currency
+const coinEL = document.querySelector('.coins');
+
+//slider variables
+const slider = document.querySelector('#rangeEL');
+const sliderOutput = document.querySelector('.time-modal-text');
+if (slider){
+    slider.oninput = () =>{
+        sliderOutput.innerHTML = slider.value + " minutes";
+        slider.addEventListener('input', () => {
+            //TODO: properly add gradient to slider
+            let x = slider.value;
+            let color = 'linear-gradient(90deg, #A885BC ' + x + '%, #BC8C85 ' + x + '%)';
+            slider.style.background = color;
+        });
+    }
+}
+
+
+
+const weekday = [
+    "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
+];
+
+
+
+//retrieve currency from local storage
+let existingCurrency = localStorage.getItem('coins');
+//if currency is null, set it to 0
+let currency = existingCurrency ? existingCurrency : 0;
+
+if (currencyEl){
+    currencyEl.innerHTML = currency;
+}
+
 let totalTime = 0;
 let timeInterval;
 let isTimerRunning = false;
 let quoteOfTheDay;
-const fetchData = async() =>{
-    const url = 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info';
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'a79c202c29msh3d60f4f1a64ce48p1aaa25jsne46a2eeb1e4c',
-            'X-RapidAPI-Host': 'quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com'
-        }
-    };
-    
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        console.log(result);
-        quoteOfTheDay = result.text;
-        document.getElementById('quote').innerHTML = quoteOfTheDay;
 
-    } catch (error) {
-        console.error(error);
-    }
+
+let studyHistoryInfo = {
+monday: {
+    'date': 'Monday',
+    'study-time': 0
+},  
+tuesday: {
+    'date': 'Tuesday',
+    'study-time': 0
+},
+wednesday: {
+    'date': 'Wednesday',
+    'study-time': 0
+},
+thursday: {
+    'date': 'Thursday',
+    'study-time': 0
+},
+friday: {
+    'date': 'Friday',
+    'study-time': 0
+},
+saturday: {
+    'date': 'Saturday',
+    'study-time': 0
+},
+sunday: {
+    'date': 'Sunday',
+    'study-time': 0
 }
-//call function to retrieve promise
-fetchData()
+}
+
+let studyArray = [];
+//object to store study history
+// const fetchData = async() =>{
+//     const url = 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info';
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'X-RapidAPI-Key': 'a79c202c29msh3d60f4f1a64ce48p1aaa25jsne46a2eeb1e4c',
+//             'X-RapidAPI-Host': 'quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com'
+//         }
+//     };
+    
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.json();
+//         console.log(result);
+//         quoteOfTheDay = result.text;
+//         document.getElementById('quote').innerHTML = quoteOfTheDay;
+
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+// //call function to retrieve promise
+// fetchData()
 
 //storing and retrieving user currency 
-let userCurrency = localStorage.getItem('carrots');
-
-if (userCurrency){
-    let userData = JSON.parse(userCurrency);
-} else {
-    localStorage.setItem(
-        'carrot', 0 
-        );
-}
-
 
 
 const startTimer = () => {
     if (totalTime === 0){
-        document.querySelector('.countdown-background').classList.toggle('active')
-        let minutes = parseInt(document.getElementById('min-input').value);
-        let seconds = parseInt(document.getElementById('sec-input').value);
-
-        //input field is 0 is nothing has been entered
-        if (isNaN(minutes)) {
-            minutes = 0;
-        }
-    
-        if (isNaN(seconds)) {
-            seconds = 0;
-        }
+        let minutes = slider.value;
+        let seconds = 0;
     
         totalTime = (minutes * 60) + seconds; //total time in seconds
     }
@@ -68,22 +127,26 @@ const startTimer = () => {
         let displayMin = (minutes < 10) ? "0" + minutes : minutes;
         let displaySec = (seconds < 10) ? "0" + seconds : seconds;
 
-        document.getElementById('countdown').innerHTML = displayMin + ':' + displaySec;
+        document.querySelector('.countdown').innerHTML = displayMin + ':' + displaySec;
         if (totalTime <= 0){
             let amountEarned = Math.floor(currencyTime / 2)
             currency+= amountEarned;
-            document.getElementById('currency').innerHTML = currency;
-            let modalContainer = document.querySelector('.currency-modal');
+            let modalContainer = currencyMessage;
             let modalElement = document.createElement("currencyMessage");
+
+            //store currency in local storage
+            localStorage.setItem('coins', currency);
+            currencyEl.innerHTML = currency;
+
+            //store study info in array, gather day of the week
+            updateHistoryArray(getStudyInfo(currencyTime));
+
             if (amountEarned > 0){
-                toggleCurrencyMessage();
-                modalElement.innerHTML += `<h3><span>${amountEarned}</span> carrots found!</h3>`;
-                modalContainer.appendChild(modalElement);
+                currencyAnimation(modalContainer, modalElement, amountEarned);
             }
 
             clearInterval(timeInterval); //stops timer setInterval
-            document.getElementById('countdown').innerHTML = "00:00"
-            document.querySelector('.countdown-background').classList.toggle('active')
+            document.querySelector('.countdown').innerHTML = "00:00"
             isTimerRunning = false;
             //displays play button again
             switchButtons();
@@ -93,33 +156,43 @@ const startTimer = () => {
         }
 
 
-     }, 1000)   //occurs every second
+     }, 10)   // 1000ms = 1s occurs every second
     
 }
 
-
 const showTimeModal = () =>{
-    document.getElementById('time-modal').classList.toggle('active');
-    document.getElementById('overlay').classList.toggle('active');
+    timePopUp.classList.toggle('active');
+    overlay.classList.toggle('active');
 }
 
 const toggleShop = () =>{
     document.querySelector('.shop-modal').classList.toggle('active');
-    document.getElementById('overlay').classList.toggle('active');
+    overlay.classList.toggle('active');
 }
 
 const toggleMenu = () =>{
     document.querySelector('.menu').classList.toggle('active');
-    document.getElementById('overlay').classList.toggle('active');
+    overlay.classList.toggle('active');
 }
 const toggleModal = () =>{
-    document.getElementById('time-modal').classList.toggle('active');
-    document.getElementById('overlay').classList.toggle('active');
+    document.querySelector('#time-modal').classList.toggle('active');
+    document.querySelector('#overlay').classList.toggle('active');
 }
 
 const toggleCurrencyMessage = () =>{
-    document.querySelector('.currency-modal').classList.toggle('active');
-    document.getElementById('overlay').classList.toggle('active');
+    currencyMessage.classList.toggle('active');
+    overlay.classList.toggle('active');
+}
+
+const currencyAnimation = (container, element, amountEarned) =>{
+    toggleCurrencyMessage();
+    element.innerHTML += `<h3><span id='coin-logo'></span><span>${amountEarned}</span> kitty kash found!</h3>`;
+    container.appendChild(element);
+
+    //TODO: add animation to coin
+    coinEL.style.animation = 'float 0.6s ease-in-out 1';
+    coinEL.addEventListener('animationend', () =>{coinEL.style.animation = ''}//resets animation
+    );
 }
 
 const switchButtons = () =>{
@@ -137,14 +210,14 @@ const pauseTimer = () =>{
     switchButtons();
 }
 
-
-document.getElementById('currency').innerHTML = currency;
-
+if (document.querySelector('.shareEl')){
 document.querySelector('.shareEl').addEventListener('click', () => {
-    document.querySelector('.share-icon-container').classList.toggle('active');
-});
+        document.querySelector('.share-icon-container').classList.toggle    ('active');
+    });
+}
 
-document.getElementById('play-button').addEventListener('click', () =>{
+if(document.querySelector('#play-button')){
+document.querySelector('#play-button').addEventListener('click', () =>{
     if (isTimerRunning) {
         // If the timer is running, just switch buttons
         switchButtons();
@@ -157,28 +230,87 @@ document.getElementById('play-button').addEventListener('click', () =>{
         showTimeModal();
     }
 });
+}
 
-document.querySelector('.hamburger-menu').addEventListener('click', toggleMenu);
-document.querySelector('.shop-icon').addEventListener('click', toggleShop);
+if(hamburgerMenu && shopIcon && document.querySelector('.shop-modal-close')){
+    hamburgerMenu.addEventListener('click', toggleMenu);
+    shopIcon.addEventListener('click', toggleShop);
 document.querySelector('.shop-modal-close').addEventListener('click', toggleShop);
+}
 
-document.querySelector('.currency-modal-close').addEventListener('click', () => {
-    toggleCurrencyMessage();
-    document.querySelector('.currency-modal').removeChild('currencyMessage');
-});
+if (document.querySelector('.currency-modal-close')){
+    document.querySelector('.currency-modal-close').addEventListener('click', () => {
+        toggleCurrencyMessage();
+        currencyMessage.removeChild('currencyMessage');
+    });
+}
+if(document.querySelector('.currency-modal-close')){
+    document.querySelector('.currency-modal-close').addEventListener('click', () => {
+        toggleCurrencyMessage();
+        currencyMessage.removeChild('currencyMessage');
+    });
+}
 
+if(document.querySelector('.time-modal-submit')){
 document.querySelector('.time-modal-submit').addEventListener('click', () => {
         startTimer();
         showTimeModal();
         switchButtons();
 });
-document.getElementById('stop-button').addEventListener('click', () => {
-    isTimerRunning = false;
-    document.querySelector('.countdown-background').classList.toggle('active');
-    document.getElementById('countdown').innerHTML = "00:00"
-    totalTime = 0;
-    clearInterval(timeInterval);
-    switchButtons();
-});
-document.getElementById('pause-button').addEventListener('click', pauseTimer);
+}
+if(document.querySelector('#stop-button')){
+    document.querySelector('#stop-button').addEventListener('click', () => {
+        isTimerRunning = false;
+        document.querySelector('.countdown').innerHTML = "00:00"
+        totalTime = 0;
+        clearInterval(timeInterval);
+        switchButtons();
+    });
+}
+if(document.querySelector('#pause-button')){
+    document.querySelector('#pause-button').addEventListener('click', pauseTimer);
+}
 
+
+const getStudyInfo = (currency) =>{
+    let d = new Date();
+    let day = weekday[d.getDay];
+    let currentStudyInfo = {
+        'date': new Date().toLocaleString('en-uk', {  weekday: 'long' }),
+        'study-time': currency
+    }
+    return currentStudyInfo;
+}
+
+
+const updateHistoryArray = (currentStudyInfo) =>{
+    switch(currentStudyInfo["date"]){
+        case 'Monday':
+            studyHistoryInfo.monday["study-time"] += currentStudyInfo["study-time"];
+            break;
+        case 'Tuesday':
+            studyHistoryInfo.tuesday["study-time"] += currentStudyInfo["study-time"];
+            break;
+        case 'Wednesday':
+            studyHistoryInfo.wednesday["study-time"] += currentStudyInfo["study-time"];;
+            break;
+        case 'Thursday':
+            studyHistoryInfo.thursday["study-time"] += currentStudyInfo["study-time"];;
+            break;
+        case 'Friday':
+            studyHistoryInfo.friday["study-time"] += currentStudyInfo["study-time"];;
+            break;
+        case 'Saturday':
+            studyHistoryInfo.saturday["study-time"] += currentStudyInfo["study-time"];;
+            break;
+        case 'Sunday':
+            studyHistoryInfo.sunday["study-time"] += currentStudyInfo["study-time"];;
+            break;
+    }
+    //for debug purposes
+    console.log(studyHistoryInfo);
+    initialStudyInfo = studyHistoryInfo;
+
+
+}
+export { initialStudyInfo };
