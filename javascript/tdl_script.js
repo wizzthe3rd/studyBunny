1dee08fc40b5a864c58287591527f687dbd1fe04
jamesojj <149243027+jamesojj@users.noreply.github.com>
2024-04-11 19:31:37 +0100

    tdl_input

--- javascript/tdl_script.js
+++ javascript/tdl_script.js
@@ -8,3 +8,19 @@ const setUpTdl = () => {
     window.location.href = "index.html";
 });
 }
+
+const inputBox = document.getElementById("tdl-input");
+const listContainer = document.querySelector(".list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        for (let i = 0; i < listContainer.length; i++) {
+            listContainer[i].appendChild(li);
+        }
+    }
+}
